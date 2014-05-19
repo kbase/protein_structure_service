144fb35ea8f3182b931a03b4fd06d3d3c5323150
Sean R. McCorkle <sean.r.mccorkle@gmail.com>
2014-05-18 22:56:44 -0500

    Tabular output for prst-lookup-pdb-by-md5.pl

--- scripts/prst-lookup-pdb-by-md5.pl
+++ scripts/prst-lookup-pdb-by-md5.pl
@@ -1,53 +1,50 @@
 #!/usr/bin/perl
 use strict;
 use Data::Dumper;
-use Carp;
 use Getopt::Long;
-use DBI;
 =head1 NAME
 
-get_go_enrichment - find out enriched GO terms in a set of genes.
+prst-lookup-pdb-by-md5 - find PDB structure matches by sequence
 
 =head1 SYNOPSIS
 
-get_go_enrichment [--url=http://kbase.us/services/ontology_service] [--domain_list=biological_process,molecular_function,cellular_component] [--evidence_code_list=IEA]  [--test_type=hypergeometric] < geneIDsList
+prst-lookup-pdb-by-md5 [--url=http://kbase.us/services/ontology_service] [--domain_list=biological_process,molecular_function,cellular_component] [--evidence_code_list=IEA]  [--test_type=hypergeometric] < geneIDsList
 
 =head1 DESCRIPTION
 
-Use this function to perform GO enrichment analysis on a set of kbase gene ids..
+Use this to determine any PDB sequences
 
 =head2 Documentation for underlying call
 
-    For a given list of kbase gene ids from a particular genome (for example "Athaliana" ) find out the significantly enriched GO terms in your gene set. This function accepts four parameters: A list of kbase gene-identifiers, a list of ontology domains (e.g."biological process", "molecular function", "cellular component"), a list of evidence codes (e.g."IEA","IDA","IEP" etc.), and test type (e.g. "hypergeometric"). The list of kbase gene identifiers cannot be empty; however the list of ontology domains and the list of evidence codes can be empty. If any of these two lists is not empty then the gene-id and the go-id pairs retrieved from KBase are further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the initial results then it is recommended to provide empty domain and evidence code lists. Final filtered list of the kbase gene-id to go-ids mapping is used to calculate GO enrichment using hypergeometric test and provides pvalues.The default pvalue cutoff is used as 0.05. Also, if input species is not provided then by default Arabidopsis thaliana is considered the input species.
+    For a given list of kbase protein MD5 ids, look for exact matches in PDB sequences, or barring that, look for near matches with blastp.  Reports matches in order of sequence similarity and structure resolution.
 
+    TODO describe any optional parameters - similariy cutoffs, etc
 
 =head1 OPTIONS
 
 =over 6
 
-=item B<-u> I<[http://kbase.us/services/ontology_service]> B<--url>=I<[http://kbase.us/services/ontology_service]>
+=item B<-u> I<[http://kbase.us/services/protein_structure_service]> B<--url>=I<[http://kbase.us/services/protein_structure_service]>
 url of the server
 
 =item B<-h> B<--help>
-prints help information
+prints help, then exits
 
 =item B<--version>
-print version information
+print version, then exits
 
-=item B<--domain_list> comman separated list of ontology domains e.g. [biological_process,molecular_function,cellular_component]
+=item B<--verbose>
+prints verbose output for debugging.
 
-=item B<--evidence_code_list> commma separated list of ontology term evidence code e.g [IEA,IEP]
-
-=item B<--test_type> statistical test to use for enrichment analysis [hypergeometric|chisq]
 
 =back
 
 =head1 EXAMPLE
 
- echo "kb|g.3899.locus.2366" | get_go_enrichment
- echo "kb|g.3899.locus.2366" | get_go_enrichment --evidence_code=[IEA,IEP]
- get_go_enrichment --help
- get_go_enrichment --version
+ echo "af1975d214436f018b10a0c161867236" | prst-lookup-pdb-by-md5
+ prst-lookup-pdb-by-md5 --help
+ prst-lookup-pdb-by-md5 --version
+ 
 
 =head1 VERSION
 
@@ -57,81 +54,96 @@ print version information
 
 use Bio::KBase::KBaseProteinStructure::KBaseProteinStructureClient;
 
-my $usage = "Usage: $0 [--url=http://kbase.us/services/protein_structure] [--domain_list=biological_process] [--evidence_code_list=IEA]  [--test_type=hypergeometric] [--p_value=XXX]< geneIDs  \n";
+#my $usage = "Usage: $0 [--url=http://kbase.us/services/protein_structure] < MD5_list \n";
+my $usage = "Usage: $0 [--help --version] < MD5_list \n";
 
-my $url        = "http://localhost:7088";
-my $type       = "hypergeometric";
-my $help       = 0;
-my $version    = 0;
-my $domainList="biological_process,molecular_function,cellular_component";
-my $ecList     = "IEA,IDA,IPI,IMP,IGI,IEP,ISS,ISS,ISO,ISA,ISM,IGC,IBA,IBD,IKR,IRD,RCA,TAS,NAS,IC,ND,NR";
-my $pvalue_cutoff="0.05";
-my $ontologytype="GO";
+#my $url        = "http://localhost:7088";
+my $url        = "http://140.221.85.122:7088";
+my $help       = '';
+my $version    = '';
+my $verbose    = '';
 
 GetOptions("help"       => \$help,
            "version"    => \$version,
-           "url=s"      => \$url#, 
-           #"domain_list=s" => \$domainList, 
-           #"evidence_code_list=s" => \$ecList,
-           #"test_type=s" => \$type,
-	#"p_value=s"=>\$pvalue_cutoff,
-#"ontology_type=s"=>\$ontologytype
-) or die $usage;
+           #"url=s"      => \$url,
+           "verbose"    => \$verbose,
+           ) or die $usage;
 
-if($help)
-{
-    print <<MAN;
-    DESCRIPTION
-         For a given list of kbase gene ids from a particular genome (for example "Athaliana" ) find out the significantly enriched GO terms in your gene set. This function accepts four parameters: A list of kbase gene-identifiers, a list of ontology domains (e.g."biological process", "molecular function", "cellular component"), a list of evidence codes (e.g."IEA","IDA","IEP" etc.), and test type (e.g. "hypergeometric"). The list of kbase gene identifiers cannot be empty; however the list of ontology domains and the list of evidence codes can be empty. If any of these two lists is not empty then the gene-id and the go-id pairs retrieved from KBase are further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the initial results then it is recommended to provide empty domain and evidence code lists. Final filtered list of the kbase gene-id to go-ids mapping is used to calculate GO enrichment using hypergeometric test and provides pvalues.The default pvalue cutoff is used as 0.05. Also, if input species is not provided then by default Arabidopsis thaliana is considered the input species.
-MAN
-
-	print "$usage\n";
-	print "\n";
-	print "General options\n";
-    print "\t--url=[http://kbase.us/services/ontology_service]\t\turl of the server\n";
-	print "\t--domain_list=[biological_process,molecular_function,cellular_component]\t\tdomain list (comma separated)\n";
-	print "\t--evidence_code_list=[XXX,YYY,ZZZ,...]\t\tGO evidence code list (comma separated)\n";
-	print "\t--test_type=[hypergeometric|chisq]\t\tthe types of test\n";
-	print "\t--help\t\tprint help information\n";
-	print "\t--version\t\tprint version information\n";
-	print "\n";
-	print "Examples: \n";
-	print "echo 'kb|g.3899.locus.2366' | $0\n";
-	print "\n\n";
-	print " echo  'kb|g.3899.locus.2366,kb|g.3899.locus.1892,kb|g.3899.locus.2354,kb|g.3899.locus.2549,kb|g.3899.locus.2420,kb|g.3899.locus.2253,kb|g.3899.locus.2229'| get_go_enrichment\n";
-	print "\n\n";
-	print "$0 --help\tprint out help\n";
-	print "\n";
-	print "$0 --version\tprint out version information\n";
-	print "\n";
-	print "Report bugs to Shinjae Yoo at sjyoo\@bnl.gov\n";
-	exit(0);
-}
-
-if($version)
-{
-	print "$0 version 0.1\n";
-	print "Copyright (C) 2012 Shinjae Yoo\n";
-	print "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n";
-	print "This is free software: you are free to change and redistribute it.\n";
-	print "There is NO WARRANTY, to the extent permitted by law.\n";
-	print "\n";
-	print "Written by Shinjae Yoo and Sunita Kumari\n";
-	exit(0);
-}
-
+if ( $help )
+   { help_then_exit(); }
+elsif ( $version )
+   { version_then_exit(); }
 die $usage unless @ARGV == 0;
 
+# establish connection to service
+
 my $psc = Bio::KBase::KBaseProteinStructure::KBaseProteinStructureClient->new($url);
-print "hello. have psc $psc\n";
-#my @dl = split/,/, $domainList;
-#my @el = split/,/, $ecList;
+print "have psc $psc\n" if ( $verbose );
+
+# read input MD5s all at once
 my @input = <STDIN>;
 my $istr = join(" ", @input);
 $istr =~ s/[,]/ /g;
 @input = split /\s+/, $istr;
-print "going for lookup\n";
-my $results = $psc->lookup_pdb_by_md5( \@input);
-print "back from lookup";
-print Dumper($results);
+
+print "performing lookup_pdb_by_md5\n" if ( $verbose );
+my $pdb_match_records = $psc->lookup_pdb_by_md5( \@input );
+if ( $verbose )
+   {
+    print "back from lookup_pdb_by_md5\n";
+    print Dumper( $pdb_match_records );
+   }
+
+# print hits
+my @fields = ( 'pdb_id', 'chains', 'resolution', 'exact', 'percent_id', 'align_len'  );
+
+foreach my $md5 ( @input )
+   {
+    print "md5 is $md5\n" if ( $verbose );
+
+    if ( my $matchl = $pdb_match_records->{$md5} )
+       {
+       	foreach my $match ( @{$matchl} )
+       	   { print join( "\t", $md5, map( $match->{$_}, @fields ) ), "\n"; }
+       }
+   }
+print "program $0 ends.\n" if ( $verbose );
+
+
+                                 ###############
+                                 # Subroutines #
+                                 ###############
+
+sub  help_then_exit
+   {
+    print <<EndOfDescription;
+    DESCRIPTION
+         For a given list of KBase MD5 protein ids, return PDB ids where the protein sequence is an exact or close match.
+EndOfDescription
+	print "$usage\n";
+	print "\n";
+	print "General options\n";
+    #print "\t--url=[http://kbase.us/services/ontology_service]\t\turl of the server\n";
+	print "\t--help\t\tprint help, then exit\n";
+	print "\t--version\t\tprint version, then exit\n";
+	print "\t--verbose\t\tverbose messages\n";
+	print "\n";
+	print "Examples: \n";
+	print "\n\n";
+    print "\techo f50a2d83cd0a3b1aa7b76ffcd4dedf40 | $0\n";
+	print "\n";
+	print "Report bugs to Sean McCorkle mccorkle\@bnl.gov\n";
+	exit(0);
+   }
+
+sub  version_then_exit
+   {
+	print "$0 version 0.1\n";
+	#print "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n";
+	#print "This is free software: you are free to change and redistribute it.\n";
+	#print "There is NO WARRANTY, to the extent permitted by law.\n";
+	print "\n";
+	print "Programmer: Sean McCorkle\n";
+	exit(0);
+   }
 
